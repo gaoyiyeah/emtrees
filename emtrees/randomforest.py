9f17e26ba1532c4ac12a5a7139f463fc3f363912
Jon Nordby <jononor@gmail.com>
2018-03-23 18:37:17 +0100

    Avoid counting samples

--- emtrees/randomforest.py
+++ emtrees/randomforest.py
@@ -15,11 +15,13 @@ def test_split(feature, value, dataset):
 
     #assert left[:,feature].max() <= value <= right[:,feature].min()
     return left, right
- 
+
+
 # Calculate the Gini index for a split dataset
 def gini_index(groups, classes):
-    # count all samples at split point
-    n_instances = float(sum([len(group) for group in groups]))
+    left, right = groups
+    n_instances = float(left.shape[0] + right.shape[0])
+
     # sum weighted Gini index for each group
     gini = 0.0
     for group in groups:
@@ -41,17 +43,21 @@ def get_split(dataset, n_features):
 
     class_values = list(numpy.unique(dataset[:,-1])) # FIXME: pass down
     b_index, b_value, b_score, b_groups = 999, 999, 999, None
+
     features = list()
     while len(features) < n_features:
         index = random.randrange(len(dataset[0])-1)
         if index not in features:
             features.append(index)
-    for index in features:
+
+    for feature in features:
         for row in dataset:
-            groups = test_split(index, row[index], dataset)
+            groups = test_split(feature, row[feature], dataset)
             gini = gini_index(groups, class_values)
+
             if gini < b_score:
-                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+                b_index, b_value, b_score, b_groups = feature, row[feature], gini, groups
+
     return {'index':b_index, 'value':b_value, 'groups':b_groups}
  
 # Create a terminal node value
